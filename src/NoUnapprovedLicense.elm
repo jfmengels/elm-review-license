00ee8b78eb0aa9434c6bdfc25fb158857e809d28
Jeroen Engels <jfm.engels@gmail.com>
2020-11-16 15:27:59 +0100

    Copy NoUnapprovedLicense rule from jfmengels/elm-review

--- src/NoUnapprovedLicense.elm
+++ src/NoUnapprovedLicense.elm
@@ -6,45 +6,166 @@ module NoUnapprovedLicense exposing (rule)
 
 -}
 
-import Review.Rule as Rule exposing (Rule)
+import Dict exposing (Dict)
+import Elm.License
+import Elm.Project
+import Elm.Syntax.Range exposing (Range)
+import Review.Project.Dependency as Dependency exposing (Dependency)
+import Review.Rule as Rule exposing (Error, Rule)
+import Set exposing (Set)
 
 
-{-| Reports... REPLACEME
+{-| Forbid the use of dependencies that use unknown or forbidden licenses.
 
     config =
         [ NoUnapprovedLicense.rule
+            { allowed = [ "BSD-3-Clause", "MIT" ]
+            , forbidden = [ "GPL-3.0-only", "GPL-3.0-or-later" ]
+            }
         ]
 
-
-## Fail
-
-    a =
-        "REPLACEME example to replace"
-
-
-## Success
-
-    a =
-        "REPLACEME example to replace"
-
-
-## When (not) to enable this rule
-
-This rule is useful when REPLACEME.
-This rule is not useful when REPLACEME.
-
-
-## Try it out
-
-You can try this rule out by running the following command:
-
-```bash
-elm-review --template jfmengels/elm-review-license/example --rules NoUnapprovedLicense
-```
-
 -}
-rule : Rule
-rule =
-    Rule.newModuleRuleSchema "NoUnapprovedLicense" ()
-        -- Add your visitors
-        |> Rule.fromModuleRuleSchema
+rule : { allowed : List String, forbidden : List String } -> Rule
+rule configuration =
+    Rule.newProjectRuleSchema "NoUnapprovedLicense" initialProjectContext
+        |> Rule.withElmJsonProjectVisitor elmJsonVisitor
+        |> Rule.withDependenciesProjectVisitor dependenciesVisitor
+        |> Rule.withFinalProjectEvaluation (finalEvaluationForProject configuration)
+        |> Rule.fromProjectRuleSchema
+
+
+type alias Configuration =
+    { allowed : List String
+    , forbidden : List String
+    }
+
+
+dependenciesVisitor : Dict String Dependency -> ProjectContext -> ( List nothing, ProjectContext )
+dependenciesVisitor dependencies projectContext =
+    let
+        licenses : Dict String String
+        licenses =
+            dependencies
+                |> Dict.toList
+                |> List.filterMap
+                    (\( packageName, dependency ) ->
+                        case Dependency.elmJson dependency of
+                            Elm.Project.Package { license } ->
+                                Just ( packageName, Elm.License.toString license )
+
+                            Elm.Project.Application _ ->
+                                Nothing
+                    )
+                |> Dict.fromList
+    in
+    ( [], { projectContext | licenses = licenses } )
+
+
+
+-- PROJECT VISITORS
+
+
+elmJsonVisitor : Maybe { elmJsonKey : Rule.ElmJsonKey, project : Elm.Project.Project } -> ProjectContext -> ( List nothing, ProjectContext )
+elmJsonVisitor maybeProject projectContext =
+    case maybeProject of
+        Just { elmJsonKey } ->
+            ( [], { projectContext | elmJsonKey = Just elmJsonKey } )
+
+        Nothing ->
+            ( [], projectContext )
+
+
+
+-- CONTEXT
+
+
+type alias ProjectContext =
+    { elmJsonKey : Maybe Rule.ElmJsonKey
+    , licenses : Dict String String
+    }
+
+
+initialProjectContext : ProjectContext
+initialProjectContext =
+    { elmJsonKey = Nothing
+    , licenses = Dict.empty
+    }
+
+
+
+-- FINAL EVALUATION
+
+
+finalEvaluationForProject : Configuration -> ProjectContext -> List (Error { useErrorForModule : () })
+finalEvaluationForProject configuration projectContext =
+    case projectContext.elmJsonKey of
+        Just elmJsonKey ->
+            let
+                allowed : Set String
+                allowed =
+                    Set.fromList configuration.allowed
+
+                forbidden : Set String
+                forbidden =
+                    Set.fromList configuration.forbidden
+
+                unknownOrForbiddenLicenses : Dict String String
+                unknownOrForbiddenLicenses =
+                    projectContext.licenses
+                        |> Dict.filter (\_ license -> not <| Set.member license allowed)
+            in
+            unknownOrForbiddenLicenses
+                |> Dict.toList
+                |> List.map
+                    (\( name, license ) ->
+                        if Set.member license forbidden then
+                            Rule.errorForElmJson elmJsonKey
+                                (\elmJson ->
+                                    { message = "Forbidden license `" ++ license ++ "` for dependency `" ++ name ++ "`"
+                                    , details = [ "This license has been marked as forbidden and you should therefore not use this package." ]
+                                    , range = findPackageNameInElmJson name elmJson
+                                    }
+                                )
+
+                        else
+                            Rule.errorForElmJson elmJsonKey
+                                (\elmJson ->
+                                    { message = "Unknown license `" ++ license ++ "` for dependency `" ++ name ++ "`"
+                                    , details =
+                                        [ "Talk to your legal team and see if this license is allowed. If it is allowed, add it to the list of allowed licenses. Otherwise, add it to the list of forbidden licenses and remove this dependency."
+                                        , "More info about licenses at https://spdx.org/licenses."
+                                        ]
+                                    , range = findPackageNameInElmJson name elmJson
+                                    }
+                                )
+                    )
+
+        Nothing ->
+            []
+
+
+findPackageNameInElmJson : String -> String -> Range
+findPackageNameInElmJson packageName elmJson =
+    elmJson
+        |> String.lines
+        |> List.indexedMap Tuple.pair
+        |> List.filterMap
+            (\( row, line ) ->
+                case String.indexes ("\"" ++ packageName ++ "\"") line of
+                    [] ->
+                        Nothing
+
+                    column :: _ ->
+                        Just
+                            { start =
+                                { row = row + 1
+                                , column = column + 2
+                                }
+                            , end =
+                                { row = row + 1
+                                , column = column + String.length packageName + 2
+                                }
+                            }
+            )
+        |> List.head
+        |> Maybe.withDefault { start = { row = 1, column = 1 }, end = { row = 10000, column = 1 } }
--- tests/NoUnapprovedLicenseTest.elm
+++ tests/NoUnapprovedLicenseTest.elm
@@ -1,24 +1,147 @@
 module NoUnapprovedLicenseTest exposing (all)
 
+import Elm.Docs
+import Elm.Project
+import Json.Decode as Decode
 import NoUnapprovedLicense exposing (rule)
+import Review.Project as Project exposing (Project)
+import Review.Project.Dependency as Dependency exposing (Dependency)
 import Review.Test
 import Test exposing (Test, describe, test)
+
+
+createProject : String -> Project
+createProject license =
+    Project.new
+        |> Project.addElmJson (createElmJson applicationElmJson)
+        |> Project.addDependency (dependency license)
+
+
+createElmJson : String -> { path : String, raw : String, project : Elm.Project.Project }
+createElmJson rawElmJson =
+    case Decode.decodeString Elm.Project.decoder rawElmJson of
+        Ok elmJson ->
+            { path = "elm.json"
+            , raw = rawElmJson
+            , project = elmJson
+            }
+
+        Err _ ->
+            Debug.todo "Invalid elm.json supplied to test"
+
+
+applicationElmJson : String
+applicationElmJson =
+    """
+{
+    "type": "application",
+    "source-directories": [
+        "src"
+    ],
+    "elm-version": "0.19.1",
+    "dependencies": {
+        "direct": {
+            "elm/core": "1.0.0",
+            "author/dependency": "1.0.0"
+        },
+        "indirect": {}
+    },
+    "test-dependencies": {
+        "direct": {},
+        "indirect": {}
+    }
+}"""
+
+
+dependency : String -> Dependency
+dependency license =
+    let
+        modules : List Elm.Docs.Module
+        modules =
+            [ { name = "Foo"
+              , comment = ""
+              , unions = []
+              , aliases = []
+              , values = []
+              , binops = []
+              }
+            ]
+
+        rawElmJson : String
+        rawElmJson =
+            """
+{
+"type": "package",
+"name": "author/dependency",
+"summary": "Summary",
+"license": \"""" ++ license ++ """",
+"version": "1.0.0",
+"exposed-modules": [
+  "Foo"
+],
+"elm-version": "0.19.0 <= v < 0.20.0",
+"dependencies": {
+  "elm/core": "1.0.0 <= v < 2.0.0"
+},
+"test-dependencies": {}
+}
+"""
+
+        elmJson : Elm.Project.Project
+        elmJson =
+            createElmJson rawElmJson
+                |> .project
+    in
+    Dependency.create
+        "author/dependency"
+        elmJson
+        modules
+
+
+sourceCode : String
+sourceCode =
+    """
+module A exposing (a)
+a = 1
+"""
 
 
 all : Test
 all =
     describe "NoUnapprovedLicense"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should not report anything if there is no `elm.json` file" <|
             \() ->
-                """module A exposing (..)
-a = 1
-"""
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
+                sourceCode
+                    |> Review.Test.run (rule { allowed = [], forbidden = [] })
+                    |> Review.Test.expectNoErrors
+        , test "should not report anything if all dependencies have a license that is allowed" <|
+            \() ->
+                sourceCode
+                    |> Review.Test.runWithProjectData (createProject "MIT") (rule { allowed = [ "MIT" ], forbidden = [] })
+                    |> Review.Test.expectNoErrors
+        , test "should report an error if a dependency has an unknown license" <|
+            \() ->
+                sourceCode
+                    |> Review.Test.runWithProjectData (createProject "BSD-3-Clause") (rule { allowed = [ "MIT" ], forbidden = [] })
+                    |> Review.Test.expectErrorsForElmJson
                         [ Review.Test.error
-                            { message = "REPLACEME"
-                            , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            { message = "Unknown license `BSD-3-Clause` for dependency `author/dependency`"
+                            , details =
+                                [ "Talk to your legal team and see if this license is allowed. If it is allowed, add it to the list of allowed licenses. Otherwise, add it to the list of forbidden licenses and remove this dependency."
+                                , "More info about licenses at https://spdx.org/licenses."
+                                ]
+                            , under = "author/dependency"
+                            }
+                        ]
+        , test "should report an error if a dependency has a forbidden license" <|
+            \() ->
+                sourceCode
+                    |> Review.Test.runWithProjectData (createProject "BSD-3-Clause") (rule { allowed = [ "MIT" ], forbidden = [ "BSD-3-Clause" ] })
+                    |> Review.Test.expectErrorsForElmJson
+                        [ Review.Test.error
+                            { message = "Forbidden license `BSD-3-Clause` for dependency `author/dependency`"
+                            , details = [ "This license has been marked as forbidden and you should therefore not use this package." ]
+                            , under = "author/dependency"
                             }
                         ]
         ]
